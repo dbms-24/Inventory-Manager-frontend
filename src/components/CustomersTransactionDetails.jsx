c3fb9033aee2e7901e52370d73b5e17095289e90
Jeevanreddy2 <155892933+Jeevanreddy2@users.noreply.github.com>
2024-11-07 13:10:00 +0530

    display transactions

--- src/components/CustomersTransactionDetails.jsx
+++ src/components/CustomersTransactionDetails.jsx
@@ -4,6 +4,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import Input from "./Input";
 import axios from "axios";
 import Get from "./useGet";
+import CustomersStock from "./CustomersStock";
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -18,31 +19,7 @@ export default function CustomersTransactionDetails() {
 
     const [open,setOpen] = useState(false);
     const [transactionsId,setTransactionsId] = useState(null);
-    const [customerTransactions,setCustomerTransactions] = useState(null);
-
-
-    const stock = data?.filter((stock)=>{
-        return (stock.id == transactionId);
-      })
-
-
-    useEffect(()=>{
-    if(customerModalId){
-        axios.get(`http://localhost:8080/transaction/customer`,{
-        'headers' : {
-            'Authorization' : token,
-            'Content-Type' : 'application/json'
-        }
-        })
-        .then((res)=>{
-        setCustomerTransactions(res.data);
-        console.log(res);
-        })
-        .catch((err)=>{
-        console.log(err)
-        })
-    }
-    },[])
+    const customerTransactions = Get(`http://localhost:8080/transaction/customer`)
     
     function handleOpen(Data){
         setOpen(true);
@@ -50,52 +27,38 @@ export default function CustomersTransactionDetails() {
       }
     
     return (
-        <>
-        <Modal open={open} onClose={()=>setOpen(false)} className="focus:outline-none">
-            <div className="flex flex-col justify-center h-screen">
-                <div className="flex justify-center">
-                <div className="bg-secondary-300 rounded-lg text-background m-10 border-2 w-1/2 px-4 pb-6">
-                    <div className="flex justify-end cursor-pointer">
-                        <CloseIcon 
-                        sx={{ color: 'red', cursor: 'pointer', fontSize: 30 }}
-                        color="font-bold text-xl mt-2" onClick={handleClose} />
-                    </div>
-                    <div className="py-8 text-center font-heading text-3xl text-primary-800">Stock</div>
+        <div className="flex justify-center">
 
-                    <CustomerTransactionStock open={open} setOpen={setOpen} transactionsId={transactionsId} setTransactionsId={setTransactionsId} data={customerTransactions} />
-                    <TableContainer component={Paper}>
-                      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                      <TableHead>
-                          <TableRow>
-                          <TableCell> (ID)</TableCell>
-                          <TableCell align="right">Customer ID</TableCell>
-                          <TableCell align="right">Transaction Date</TableCell>
-                          <TableCell align="right">Amount</TableCell>
-                          </TableRow>
-                      </TableHead>
-                      <TableBody>
-                          {customerTransactions && customerTransactions?.map((Data) => (
-                          <TableRow
-                              key={Data.id}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                              onClick={handleOpen(Data)} 
-                          >
-                              <TableCell component="th" scope="row">
-                              {Data.id}
-                              </TableCell>
-                              <TableCell align="right">{Data.customer_id}</TableCell>
-                              <TableCell align="right">{Data.transaction_date}</TableCell>
-                              <TableCell align="right">{Data.amount}</TableCell>
-                          </TableRow>
-                          ))}
-                      </TableBody>
-                      </Table>
-                  </TableContainer>
-                    
-                </div>
-                </div>
-            </div>
-        </Modal>
-        </>
+            <CustomersStock open={open} setOpen={setOpen} transactionsId={transactionsId} setTransactionsId={setTransactionsId} data={customerTransactions} />
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                    <TableCell> (ID)</TableCell>
+                    <TableCell align="right">Customer ID</TableCell>
+                    <TableCell align="right">Transaction Date</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {customerTransactions && customerTransactions?.map((Data) => (
+                    <TableRow
+                        key={Data.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        onClick={handleOpen(Data)} 
+                    >
+                        <TableCell component="th" scope="row">
+                        {Data.id}
+                        </TableCell>
+                        <TableCell align="right">{Data.customer_id}</TableCell>
+                        <TableCell align="right">{Data.transaction_date}</TableCell>
+                        <TableCell align="right">{Data.amount}</TableCell>
+                    </TableRow>
+                    ))}
+                </TableBody>
+                </Table>
+            </TableContainer>
+                
+        </div>
     )
 }
--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import AddCustomerTransaction from '../components/AddCustomerTransaction.jsx';
+import CustomersTransactionDetails from "../components/CustomersTransactionDetails"
+import SuppliersTransactionDetails from "../components/SuppliersTransactionDetails"
 
 function Transactions() {
     const [customerAddOpen, setCustomerAddOpen] = useState(false);
     return (
       <div className="min-h-screen">
       <AddCustomerTransaction customerAddOpen={customerAddOpen} setCustomerAddOpen={setCustomerAddOpen}/>
-        <div>
+        
         <div className={"flex justify-between px-10"}>
           <div className="flex flex-col justify-center font-heading text-2xl font-bold my-4">
             <div>
@@ -15,15 +17,22 @@ function Transactions() {
           </div>
         <div className="bg-primary-400 font-normal rounded-lg my-4 px-3 py-2 cursor-pointer" onClick={()=>setCustomerAddOpen(true)}>Add Customer Transaction</div>
         </div>
+        <CustomersTransactionDetails />
 
+        <div className="mt-6"></div>
 
+        <div className={"flex justify-between px-10"}>
+          <div className="flex flex-col justify-center font-heading text-2xl font-bold my-4">
+            <div>
+            Supplier Transactions
+          </div>
+          </div>
+        <div className="bg-primary-400 font-normal rounded-lg my-4 px-3 py-2 cursor-pointer" onClick={()=>setCustomerAddOpen(true)}>Add Supplier Transaction</div>
         </div>
-        <div>
-
+        <SuppliersTransactionDetails />
         </div>
-
-      </div>
+      
     )
   }
   
-  export default Transactions
+  export default Transactions;
